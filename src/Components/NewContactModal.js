ba07f813a55014a9eb1426d1fece67215557b6c1
Alexander <alex.wood1722@gmail.com>
2022-08-28 10:03:14 -0700

    Can now add contacts

--- src/Components/NewContactModal.js
+++ src/Components/NewContactModal.js
@@ -1,31 +1,34 @@
 import React, { useRef } from 'react'
 import { Form, Modal, Button } from 'react-bootstrap'
+import { useContacts } from '../Contexts/ContactsProvider'
 
-export default function NewContactModal({ closedModal }) {
+export default function NewContactModal({ closeModal }) {
     const idRef = useRef()
     const nameRef = useRef()
+    const { createContact } = useContacts()
 
     function handleSubmit(e) {
         e.preventDefault()
 
         createContact(idRef.current.value, nameRef.current.value)
-        closedModal()
+        closeModal()
     }
 
     return (
         <>
             <Modal.Header closeButton>Create Contact</Modal.Header>
             <Modal.Body>
-                <Form onSubmit={handleSubmit}></Form>
-                <Form.Group>
-                    <Form.Label>Id</Form.Label>
-                    <Form.Control type="text" ref={idRef} required />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" ref={nameRef} required />
-                </Form.Group>
-                <Button type="submit">Create</Button>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Id</Form.Label>
+                        <Form.Control type="text" ref={idRef} required />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" ref={nameRef} required />
+                    </Form.Group>
+                    <Button className="mt-3" type="submit">Create</Button>
+                </Form>
             </Modal.Body>
         </>
     )
